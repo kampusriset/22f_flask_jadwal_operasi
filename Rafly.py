54e3e7c7e34d0bfd9ef64a1eac858003f8bfbfea
ferdiharvan <169017233+ferdiharvan@users.noreply.github.com>
2025-02-19 21:32:45 +0700

    Update Rafly.py

--- Rafly.py
+++ Rafly.py
@@ -6,5 +6,5 @@ def perkenalan(nama, nim, peran):
 if name == " main ":
     nama = "Muh Rafly Fatchurohman"
     nim = "2213010375"
-    peran = "Mengerjakan semua bersama"
+    peran = "mengerjakan halaman login, change password, register"
     perkenalan(nama, nim, peran)
