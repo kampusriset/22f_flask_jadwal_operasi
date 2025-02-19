334e35540cc799cae5c42156afe4e6090642a291
RAFLY4F <muhrafli172001@gmail.com>
2025-02-19 20:24:13 +0700

    Add files via upload

---
+++ Rafly.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if name == " main ":
+    nama = "Muh Rafly Fatchurohman"
+    nim = "2213010375"
+    peran = "Mengerjakan semua bersama"
+    perkenalan(nama, nim, peran)
