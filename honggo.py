61c44364cfb4eff63cb344a4f587e487b1c35efc
GossMarket <akubakti435@gmail.com>
2025-02-19 22:17:00 +0700

    honggo.py

---
+++ honggo.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if name == " main ":
+    nama = "Honggo Negoro Seno Sri Satrio"
+    nim = "2213010376"
+    peran = "mengerjakan halaman login, change password, register"
+    perkenalan(nama, nim, peran)
